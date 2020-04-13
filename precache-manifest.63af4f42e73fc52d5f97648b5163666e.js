5414952a8b4d0a29ced8ddb1db9a977935f0e8a3
Sampaio Leal <samp14hd@gmail.com>
2020-04-13 09:25:30 -0300

    Updates

---
+++ precache-manifest.63af4f42e73fc52d5f97648b5163666e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83018e8e9ba010caa3bd0b4041541e09",
+    "url": "/Pedra-Papel-Tesoura/index.html"
+  },
+  {
+    "revision": "4e334d4fa50b736dd2f3",
+    "url": "/Pedra-Papel-Tesoura/static/css/main.95e49776.chunk.css"
+  },
+  {
+    "revision": "217c949a5cca941463ac",
+    "url": "/Pedra-Papel-Tesoura/static/js/2.a9a14803.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pedra-Papel-Tesoura/static/js/2.a9a14803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e334d4fa50b736dd2f3",
+    "url": "/Pedra-Papel-Tesoura/static/js/main.abd3a490.chunk.js"
+  },
+  {
+    "revision": "db3caa4b039ff3b21a29",
+    "url": "/Pedra-Papel-Tesoura/static/js/runtime-main.4899b58a.js"
+  }
+]);
